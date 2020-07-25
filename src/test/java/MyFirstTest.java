121c1e03906a68d329399f801a7f0b75e413332b
Alex Candido <alexcandido10@gmail.com>
2020-07-25 18:39:42 -0300

    Updating tests

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,7 +3,11 @@ import org.junit.jupiter.api.Test;
 
 public class MyFirstTest {
     @Test
-    public void assetTrue() {
+    public void assertTrue() {
         Assertions.assertTrue(true);
+    }
+
+    public void assertFalse() {
+        Assertions.assertFalse(false);
     }
 }
