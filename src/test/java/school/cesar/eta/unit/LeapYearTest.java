61e85f629e386da2692c1a4c43875ad4b08dca16
Alex Candido <alexcandido10@gmail.com>
2020-07-25 17:57:27 -0300

    Adding method check_leapYearDivisibleBy4AndDivisibleBy100_false and refactoring some test methods name

--- src/test/java/school/cesar/eta/unit/LeapYearTest.java
+++ src/test/java/school/cesar/eta/unit/LeapYearTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 public class LeapYearTest {
 
     @Test
-    public void check_leapYearDiv400_true(){
+    public void check_leapYearDivisibleBy400_true(){
         int year = 2000;
         boolean result = LeapYear.check(year);
         Assertions.assertTrue(result);
     }
 
     @Test
-    public void check_leapYearDiv4AndNotDiv100_true(){
+    public void check_leapYearDivisibleBy4AndNotDivisibleBy100_true(){
         int year = 2020;
         boolean result = LeapYear.check(year);
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void check_leapYearDivisibleBy4AndDivisibleBy100_false(){
+        int year = 100;
+        boolean result = LeapYear.check(year);
+        Assertions.assertFalse(result);
     }
 
     @Test
