8ecaf5f3ec899b22481c6a3142db6c898dffa5b8
Alex Candido <alexcandido10@gmail.com>
2020-07-25 16:31:59 -0300

    Adding check method for LeapYear class

---
+++ src/main/java/school/cesar/eta/unit/LeapYear.java
@@ -0,0 +1,18 @@
+package school.cesar.eta.unit;
+
+public class LeapYear {
+
+    public static Boolean check(int year){
+        boolean isLeapYear = false;
+
+        if(year % 400 == 0){
+            isLeapYear = true;
+        }
+
+        if((year % 4 == 0) && (year % 100 != 0)){
+            isLeapYear = true;
+        }
+
+        return isLeapYear;
+    }
+}
