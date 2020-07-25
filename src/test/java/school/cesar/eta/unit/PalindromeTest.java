e3210583fe8eff00169f70b40663fed49bb3b12f
Alex Candido <alexcandido10@gmail.com>
2020-07-25 11:15:27 -0300

    Adding Palindrome tests

--- src/test/java/school/cesar/eta/unit/PalindromeTest.java
+++ src/test/java/school/cesar/eta/unit/PalindromeTest.java
@@ -1,4 +1,28 @@
 package school.cesar.eta.unit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PalindromeTest {
+
+    @Test
+    public void check_palindromeWord_true() {
+        String word = "ANA";
+        boolean result = Palindrome.check(word);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void check_notPalindromeWord_false() {
+        String word = "ABC";
+        boolean result = Palindrome.check(word);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void check_emptyWord_true() {
+        String word = "";
+        boolean result = Palindrome.check(word);
+        Assertions.assertTrue(result);
+    }
 }
