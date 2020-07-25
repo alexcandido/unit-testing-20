c9c4e8d6d5e2689f2c8f3446f68dde09b8b0b94d
Alex Candido <alexcandido10@gmail.com>
2020-07-25 10:43:47 -0300

    Adding first test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    @Test
+    public void assetTrue(){
+        Assertions.assertTrue(true);
+    }
+}
