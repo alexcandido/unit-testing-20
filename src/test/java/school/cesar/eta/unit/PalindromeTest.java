0e3f0367bb6786bab4a6c6c347407e8f19975ea7
Alex Candido <alexcandido10@gmail.com>
2020-07-25 12:07:33 -0300

    Refactoring - initiate palindrome instance before each test

--- src/test/java/school/cesar/eta/unit/PalindromeTest.java
+++ src/test/java/school/cesar/eta/unit/PalindromeTest.java
@@ -1,28 +1,42 @@
 package school.cesar.eta.unit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeTest {
+    private Palindrome palindrome;
+
+    @BeforeEach
+    public void setup(){
+        palindrome = new Palindrome();
+    }
 
     @Test
     public void check_palindromeWord_true() {
         String word = "ANA";
-        boolean result = Palindrome.check(word);
+        boolean result = palindrome.check(word);
         Assertions.assertTrue(result);
     }
 
     @Test
     public void check_notPalindromeWord_false() {
         String word = "ABC";
-        boolean result = Palindrome.check(word);
+        boolean result = palindrome.check(word);
         Assertions.assertFalse(result);
     }
 
     @Test
     public void check_emptyWord_true() {
         String word = "";
-        boolean result = Palindrome.check(word);
+        boolean result = palindrome.check(word);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void check_oneCharWord_true() {
+        String word = "A";
+        boolean result = palindrome.check(word);
         Assertions.assertTrue(result);
     }
 }
